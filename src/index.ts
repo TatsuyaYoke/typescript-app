5e13ce1180247421a40ea9b1ba34ae8cb40cd54c
Yoke <tayok@synspective.com>
2022-05-24 14:17:57 +0900

    parse bigquery data

--- src/index.ts
+++ src/index.ts
@@ -73,6 +73,17 @@ const bigquery = new BigQuery({
   keyFilename: 'G:/共有ドライブ/0705_Sat_Dev_Tlm/settings/strix-tlm-bq-reader-service-account.json',
 })
 
+type apiSuccess<T> = { success: true; tlmId: number; data: T }
+type apiError = { success: false; tlmId: number; error: string }
+type apiReturnType<T> = apiSuccess<T> | apiError
+const regexBigQueryDateTime =
+  /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])T([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9].[0-9]{3}Z/
+const bigqueryDateType = z.object({ value: z.string().regex(regexBigQueryDateTime) })
+const bigqueryDataTypeSchema = z.union([z.number().nullable(), z.string(), bigqueryDateType])
+const bigqueryObjectDataTypeSchema = z.record(bigqueryDataTypeSchema)
+const bigqueryObjectArrayDataTypeSchema = z.array(bigqueryObjectDataTypeSchema)
+type BigQueryObjectArrayDataType = z.infer<typeof bigqueryObjectArrayDataTypeSchema>
+
 const bigqueryErrorSchema = z.object({
   reason: z.string(),
   location: z.string(),
@@ -81,27 +92,38 @@ const bigqueryErrorSchema = z.object({
 
 console.time('test')
 Promise.all(
-  querSingleTableList.map((element) => {
+  querSingleTableList.map((element): Promise<apiReturnType<BigQueryObjectArrayDataType>> => {
     return bigquery
       .query(element.query)
       .then((data) => {
+        console.log(data[0])
+        const schemaResult = bigqueryObjectArrayDataTypeSchema.safeParse(data[0])
+        if (schemaResult.success)
+          return {
+            success: true,
+            tlmId: element.tlmId,
+            data: schemaResult.data,
+          } as const
+
         return {
-          succuss: true,
+          success: false,
           tlmId: element.tlmId,
-          data: data[0],
-        }
+          error: schemaResult.error.message,
+        } as const
       })
       .catch((err) => {
         const errorParseResult = bigqueryErrorSchema.safeParse(err.errors[0])
         return {
           success: false,
           tlmId: element.tlmId,
-          error: errorParseResult.success ? errorParseResult.data : 'Cannot parse error message',
+          error: errorParseResult.success ? errorParseResult.data.message : 'Cannot parse error message',
         }
       })
   })
 ).then((response) => {
-  const res = response[1]
-  console.log(res)
+  const res = response[0]
+  if (res && res.success) {
+    console.log(res.data)
+  }
   console.timeEnd('test')
 })
