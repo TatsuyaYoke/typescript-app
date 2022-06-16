951b8643a528f151efe8cd4bc2cd75b5a0cf6687
Yoke <tayok@synspective.com>
2022-06-16 16:59:21 +0900

    add tupleToObject

--- src/type-challengs/tupleToObject.ts
+++ src/type-challengs/tupleToObject.ts
@@ -1,5 +1,6 @@
 ;(() => {
   const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+  type TupleToObject<T extends readonly (string | number)[]> = { [K in T[number]]: K }
   type result = TupleToObject<typeof tuple>
   // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
 })()
