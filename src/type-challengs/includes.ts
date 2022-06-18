a0128d23c8d810169a8a3df753872745fe2d3462
Yoke <tayok@synspective.com>
2022-06-18 12:01:24 +0900

    includes2

--- src/type-challengs/includes.ts
+++ src/type-challengs/includes.ts
@@ -1,4 +1,11 @@
 ;(() => {
-  type Includes<T extends unknown[], K> = K extends T[number] ? true : false
+  //   type Includes<T extends unknown[], K> = K extends T[number] ? true : false
+  type Equal<X, Y> = (<T>() => T extends X ? true : false) extends <T>() => T extends Y ? true : false ? true : false
+  type Includes<T extends unknown[], U> = T extends [infer First, ...infer Rest]
+    ? Equal<First, U> extends true
+      ? true
+      : Includes<Rest, U>
+    : false
+
   type isPillarMen = Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'> // expected to be `false`
 })()
